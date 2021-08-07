9a30757ed7cf06a8c83a4b681f44fe294ec8039e
caroline223 <67647836+caroline223@users.noreply.github.com>
2021-08-07 15:28:00 -0400

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,1 +1,28 @@
+class Artist
 
+    attr_reader :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def songs
+        Song.all.filter{ |song| song.artist == self }
+    end
+
+    def new_song(name, genre)
+       song = Song.new(name, self, genre)
+    end
+
+    def genres
+        self.songs.map(&:genre)
+     end
+
+end
--- lib/genre.rb
+++ lib/genre.rb
@@ -1,1 +1,25 @@
+class Genre
 
+    attr_reader :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def songs
+        Song.all.filter {|song| song.genre == self}
+    end
+
+    def artists
+       self.songs.map(&:artist)
+    end
+
+
+end
